da49d173d46e77c5244c5d4813d992eb54f9c07e
hackthsys <0xhackthesys@gmail.com>
2024-03-04 20:23:59 +0100

    Navbar fix

--- src/components/Navbar/page.jsx
+++ src/components/Navbar/page.jsx
@@ -3,13 +3,21 @@ import Link from "next/link"
 
 function Navbar() {
   return (
-    <div>
-        <nav>
-            <ul>
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/about">About</Link></li>
-                <li><Link href="/contact">Contact</Link></li>
-            </ul>   
+    <div className="flex w-full bg-red-700">
+        <nav className="flex gap-7 w-full bg-yellow-400">
+
+            <div className="bg-gray-600 w-full ml-2 justify-start flex ">
+                <h1>Logo</h1>
+            </div>
+
+            <div>
+                <ul className="flex bg-orange-300 gap-7 justify-end mr-2">
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/about">About</Link></li>
+                    <li><Link href="/contact">Contact</Link></li>
+                </ul> 
+            </div>  
+
         </nav>
     </div>
   )
