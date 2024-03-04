df0856331833138c83cc9264e0c8b024530bc6c0
hackthesys <0xhackthesys@gmail.com>
2024-03-04 18:47:46 +0100

    Website Testing

---
+++ src/about/pages.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const aboutPage = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Welcome to the About page of my app!</p>
+    </div>
+  );
+};
+
+export default aboutPage;
---
+++ src/components/Navbar/page.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link"
+
+
+function Navbar() {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/about">About</Link></li>
+                <li><Link href="/contact">Contact</Link></li>
+            </ul>   
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
